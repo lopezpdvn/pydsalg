00d9137b8a9d00207fedabad0d6fd4d5cdeedf00
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-11-21 15:17:18 -0600

    Implemented heapsort0

--- pydsalg/datastruct/heap.py
+++ pydsalg/datastruct/heap.py
@@ -1,2 +1,32 @@
-def heap_sort(x):
-    x.sort()
+def heapsort0(a):
+    def heapify(a):
+        count = len(a)
+        ilastnode = len(a) - 1
+        start = (ilastnode - 1) // 2
+
+        while start >= 0:
+            trickle_down(a, start, count)
+            start -= 1
+
+    def trickle_down(a, start, count):
+        root = start
+
+        while root * 2 + 1 < count:
+            child = root * 2 + 1
+
+            if child + 1 < count and a[child] < a[child+1]:
+                child += 1
+
+            if a[root] < a[child]:
+                a[root], a[child] = a[child], a[root]
+                root = child
+            else:
+                return
+
+    heapify(a)
+    end = len(a) - 1
+
+    while end > 0:
+        a[end], a[0] = a[0], a[end]
+        trickle_down(a, 0, end)
+        end -= 1
--- pydsalg/tests/datastruct/test_heap.py
+++ pydsalg/tests/datastruct/test_heap.py
@@ -2,9 +2,7 @@ import unittest
 from pprint import pprint
 from random import randint
 
-from pysyspol.util import random_alphanumeric_str
-
-from pydsalg.datastruct.heap import heap_sort
+from pydsalg.datastruct.heap import heapsort0
 
 class TestHeap(unittest.TestCase):
     @classmethod
@@ -15,8 +13,9 @@ class TestHeap(unittest.TestCase):
         sorted_three = [000, 333, 999]
         sorted_four = [111, 444, 555, 666]
         sorted_empty = []
+        a = [1,2,3,4,5,6,7,8]
         cls.sorted_arrs = [sorted_one, sorted_two, sorted_three, sorted_four,
-                sorted_big, sorted_empty]
+                sorted_big, sorted_empty, a]
 
     def setUp(self):
         unsorted_big = [888, 222, 333, 000, 999, 777, 555, 111, 666, 444]
@@ -25,12 +24,13 @@ class TestHeap(unittest.TestCase):
         unsorted_three = [333, 000, 999]
         unsorted_four = [555, 111, 666, 444]
         unsorted_empty = []
+        a = [6,5,3,1,8,7,2,4]
         self.unsorted_arrs = [unsorted_one, unsorted_two, unsorted_three,
-                unsorted_four, unsorted_big, unsorted_empty]
+                unsorted_four, unsorted_big, unsorted_empty, a]
 
-    def test_heap_sort(self):
+    def test_heapsort0_1(self):
         for u, s in zip(self.unsorted_arrs, self.sorted_arrs):
-            heap_sort(u)
+            heapsort0(u)
             self.assertEqual(u, s)
 
 if __name__ == '__main__':
